3519b16852f8d81e1f8daaa48f2b43e2158b5fc4
Amaya Reed <122130101+mymy777@users.noreply.github.com>
2023-12-19 11:25:57 -0800

    Player scene

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var velocity = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position =+ velocity * delta
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1 
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+		
+	
